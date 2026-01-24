6ee096a54f6f5acc8e0f760e6b6921169d6f2fc0
Michal <mpopiel0x00@gmail.com>
2026-01-24 15:19:59 +0100

    HARD PROJECT RESET

--- rag-eval-harness/src/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
-OPENAI_EMBED_MODEL = os.getenv("OPENAI_EMBED_MODEL", "text-embedding-3-small")
-QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
-QDRANT_COLLECTION = os.getenv("QDRANT_COLLECTION", "docs_v1")
